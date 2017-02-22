3dd65db51f5126478c130649ef28511c02c86e7e
badoi <badoi@me.com>
2017-02-22 11:26:00 -0500

    intron gross expression analysis

---
+++ intron/analyze_intron_timecourse.R
@@ -0,0 +1,51 @@
+# intron expression in human stem cells
+dir = '/dcl01/lieber/ajaffe/Emily/RNAseq-pipeline/Projects/AZpilot_stemcell/'
+
+##########################################
+# load data files from intron featureCounts
+load('../prep_samples/annotated_phenotype_data_stemCellTimecourse.rda')
+load(paste0(dir,'intron_rpkmCounts_AZpilot_feb22.hg38.intron_n506.rda'))
+
+#################################
+# take only biological replicates
+bioInd = which(pd$Class == "Naked genomes")
+pd = pd[bioInd,]
+pd$DAY = as.numeric(pd$DAY)
+pd$DX = factor(pd$DX,levels = c('CNT','SCZ'))
+pd$Donor = factor(pd$Donor)
+pd$totalAssignedIntron = metrics[pd$SAMPLE_ID,'totalAssignedIntron']
+
+intronRpkm = intronRpkm[,pd$SAMPLE_ID]
+
+##############################
+# filter intron RPKM dataframe
+keepIndex = which(rowMeans(intronRpkm)>0.1)
+intronRpkm = intronRpkm[keepIndex,]
+
+#################
+# make some plots
+library(RColorBrewer)
+library(ggplot2)
+pca = prcomp(t(intronRpkm))
+
+pdf('stem_cell_intron_timecourse_expression.pdf')
+ggplot(data = data.frame(pca$x,pd))+
+  geom_point(aes(x=PC1,y=PC2,fill=factor(DAY),shape = DX))+
+  scale_fill_brewer(palette = 'PiYG') + 
+  scale_shape_manual(values = c(21,24))
+
+ggplot(data = data.frame(pca$x,pd),aes(x=DAY,y=PC1))+
+  geom_point(pch=21,aes(fill=Donor),position = position_dodge(width = .25))+
+  scale_fill_brewer(palette = 'Set1') 
+
+pd$meanRpkm = colMeans(intronRpkm)
+ggplot(data = pd,aes(x=DAY,y=meanRpkm,color = Donor))+
+  geom_line()+ scale_colour_brewer(palette = 'Set1') +
+  ggtitle('Mean RPKM')
+
+ggplot(data = pd,aes(x=DAY,y=totalAssignedIntron,color = Donor))+
+  geom_line()+ scale_colour_brewer(palette = 'Set1') +
+  ggtitle('Intron Assignment Rate')
+dev.off()
+
+
